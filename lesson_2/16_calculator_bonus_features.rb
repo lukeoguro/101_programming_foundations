f56d1a77169902a90d118dc4798a837d11ad1127
Luke Oguro <lukeoguro@gmail.com>
2021-04-16 20:08:55 +0900

    [+] Add 16_calculator_bonus_features.rb in lesson_2

---
+++ lesson_2/16_calculator_bonus_features.rb
@@ -0,0 +1,121 @@
+require 'yaml'
+
+MESSAGES = YAML.load_file('16_calculator_messages.yml')
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def valid_number?(num)
+  num.to_i() != 0
+end
+
+# def number?(num)
+#   if num.include?('.')
+#     num.to_f().to_s() == num
+#   else
+#     num.to_i().to_s() == num
+#   end
+# end
+
+def operation_to_message(operator)
+  result = case operator
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+
+  result
+end
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
+prompt(messages('welcome'))
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt(messages('valid_name'))
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}!")
+
+loop do
+  number1 = ''
+  loop do
+    prompt("What's the first number?")
+    number1 = Kernel.gets().chomp()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt("What's the second number?")
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm... that doesn't look like a valid number")
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3 or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result =  case operator
+            when '1'
+              number1.to_i() + number2.to_i()
+            when '2'
+              number1.to_i() - number2.to_i()
+            when '3'
+              number1.to_i() * number2.to_i()
+            when '4'
+              number1.to_f() / number2.to_f()
+            end
+
+  Kernel.puts("The result is #{result}")
+
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for using the calculator. Good bye!")
