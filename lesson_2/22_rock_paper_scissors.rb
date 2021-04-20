66e11a5dd7b18d02497cc5887be77838548c2129
Luke Oguro <lukeoguro@gmail.com>
2021-04-20 18:15:03 +0900

    [+] Add 22_rock_paper_scissors.rb in lesson_2

---
+++ lesson_2/22_rock_paper_scissors.rb
@@ -0,0 +1,45 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def display_results(player, computer)
+  if  (player == 'rock' && computer == 'scissors') ||
+      (player == 'paper' && computer == 'rock') ||
+      (player == 'scissors' && computer == 'paper')
+    prompt("You won!")
+  elsif (player == 'scissors' && computer == 'rock') ||
+        (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors')
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}:")
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = ['rock', 'paper', 'scissors'].sample
+
+  Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for playing. Good bye!")
