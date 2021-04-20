a3c3d70a042faaed3b2dda2c47a3709137d814c0
Luke Oguro <lukeoguro@gmail.com>
2021-04-20 17:29:39 +0900

    [e] Refactor 17_mortgage_calculator.rb in lesson_2

--- lesson_2/17_mortgage_calculator.rb
+++ lesson_2/17_mortgage_calculator.rb
@@ -1,4 +1,7 @@
-# Basic validation methods
+require 'yaml'
+
+MESSAGES = YAML.load_file('17_mortgage_calculator.yml')
+
 def not_empty?(str)
   str.delete(' ') != ''
 end
@@ -11,13 +14,13 @@ def float?(str)
   str.to_f.to_s == str
 end
 
-# Consolidates validation methods for loan, interest, year, month
 def valid_loan_amount?(str)
   not_empty?(str) && integer?(str) && str.to_i.positive?
 end
 
 def valid_apr?(str)
-  not_empty?(str) && (integer?(str) || float?(str)) && str.to_f.positive?
+  not_empty?(str) && (integer?(str) || float?(str)) &&
+    (str.to_f.positive? || str.to_f.zero?)
 end
 
 def valid_year?(str)
@@ -28,8 +31,7 @@ def valid_month?(str)
   not_empty?(str) && integer?(str) && str.to_i.between?(0, 11)
 end
 
-# Prompt and format methods
-def prompt(message)
+def format_and_prompt(message)
   message.split("\n").each_with_index do |line, idx|
     idx == 0 ? (puts "=> #{line}") : (puts "=>   #{line}")
   end
@@ -43,79 +45,86 @@ def format_percent(num)
   "#{format('%.3f', num * 100)}%"
 end
 
-# Methods to get user input
 def get_loan_amount
   loop do
-    prompt("Enter the total loan amount to the nearest dollar (e.g., 100,000):")
+    format_and_prompt(MESSAGES[:get_loan_amount])
     loan_amount = gets.chomp.delete("$").delete(",")
-    return loan_amount.to_i if valid_loan_amount?(loan_amount)
-    prompt("Please enter a positive, non-zero whole number.")
+    return loan_amount.to_f if valid_loan_amount?(loan_amount)
+    format_and_prompt(MESSAGES[:get_loan_amount_error])
   end
 end
 
 def get_apr
   loop do
-    prompt("Enter the APR (e.g., 6.13%);")
+    format_and_prompt(MESSAGES[:get_apr])
     apr = gets.chomp.delete("%")
     return apr.to_f * 0.01 if valid_apr?(apr)
-    prompt("Please enter a positive, non-zero number.")
+    format_and_prompt(MESSAGES[:get_apr_error])
   end
 end
 
 def get_year
   loop do
-    prompt("Enter the loan duration, starting with the number of years:")
+    format_and_prompt(MESSAGES[:get_year])
     year = gets.chomp
     return year.to_i if valid_year?(year)
-    prompt("Please enter a non-negative whole number.")
+    format_and_prompt(MESSAGES[:get_year_error])
   end
 end
 
 def get_month
   loop do
-    prompt("Now enter the number of month:")
+    format_and_prompt(MESSAGES[:get_month])
     month = gets.chomp
     return month.to_i if valid_month?(month)
-    prompt("Please enter a valid whole number between 0 and 11.")
+    format_and_prompt(MESSAGES[:get_month_error])
+  end
+end
+
+def get_year_month
+  loop do
+    year = get_year
+    month = get_month
+    return year, month unless year == month && year == 0
+    format_and_prompt(MESSAGES[:get_year_month_error])
   end
 end
 
 def get_repeat
   loop do
-    prompt("Would you like to run the program again? (Y/N)")
+    format_and_prompt(MESSAGES[:get_repeat])
     repeat = gets.chomp.downcase
     if repeat == "yes" || repeat == "y"
       return true
     elsif repeat == "no" || repeat == "n"
       return false
     end
-    prompt("Please enter Y/N.")
+    format_and_prompt(MESSAGES[:get_repeat_error])
   end
 end
 
-# Consolidates month and year user input methods for duration
-def get_total_months
-  loop do
-    year = get_year
-    month = get_month
-    total_months = year * 12 + month
-    return year, month, total_months if total_months > 0
-    prompt("Please enter a valid loan duration (i.e., not 0 years, 0 months).")
+def calc_monthly_payment(loan_amount, monthly_interest, total_months)
+  if monthly_interest == 0
+    loan_amount / total_months
+  else
+    loan_amount * (monthly_interest / (1 - (1 + monthly_interest)**\
+      (-total_months)))
   end
 end
 
-# Main mortgage calculator loop
 loop do
   system("clear") || system("cls")
-  prompt("Welcome to the mortgage calculator!")
+  format_and_prompt(MESSAGES[:welcome])
 
-  loan_amount = get_loan_amount # int
+  loan_amount = get_loan_amount # float
   apr = get_apr # float (e.g., 0.06 for 6%)
-  year, month, total_months = get_total_months # int
+  year, month = get_year_month # int
 
   monthly_interest = apr / 12
-  monthly_payment = loan_amount * (monthly_interest / \
-                    (1 - (1 + monthly_interest)**(-total_months)))
+  total_months = year * 12 + month
+  monthly_payment = calc_monthly_payment(loan_amount,
+                                         monthly_interest,
+                                         total_months)
 
   output_message = "Results for a loan of #{format_cash(loan_amount)} "\
   "at #{format_percent(apr)} APR over #{year} year(s) and #{month} month(s):\n"\
@@ -123,9 +132,9 @@ loop do
   "Number of payments: #{total_months}\n"\
   "Monthly payment: #{format_cash(monthly_payment)}\n"
 
-  prompt(output_message)
+  format_and_prompt(output_message)
 
   break unless get_repeat
 end
 
-prompt("Thank you for using the mortgage calculator!")
+format_and_prompt(MESSAGES[:thank_you])
